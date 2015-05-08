b10156881cbdab16edf8f3f6d53e2538595b22cc
gigigigi <gigigigi>
2015-05-09 03:12:53 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+gitbook build
+cp -a _book /tmp/
+git add .
+git commit -m 'update'
+git push
+git checkout gh-pages
+cp /tmp/_book/* .
+git add .
+git commit -m 'update'
+git push
+#git checkout master

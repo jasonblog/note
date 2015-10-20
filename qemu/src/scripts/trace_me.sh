8f3bafe867337dd56c9e6626d7d9b6c7804eba06
gigi <gigi>
2015-10-21 00:12:24 +0800

    Update documentation

---
+++ qemu/src/scripts/trace_me.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+pause() {
+    read -n 1 -p "$*" INP
+    if [ "$INP" != '' ] ; then
+        echo -ne '\b \n'
+    fi
+}
+
+if [ ! -d "/sys/kernel/debug/tracing" ]; then
+mount -t debugfs none /sys/kernel/debug
+fi
+
+echo nop > /sys/kernel/debug/tracing/current_tracer
+echo 16384 > /sys/kernel/debug/tracing/buffer_size_kb
+echo 0 > /sys/kernel/debug/tracing/tracing_on
+echo  > /sys/kernel/debug/tracing/trace
+pause 'press any key to start capturing...'
+
+echo function_graph > /sys/kernel/debug/tracing/current_tracer
+echo funcgraph-proc > /sys/kernel/debug/tracing/trace_options
+echo 1 > /sys/kernel/debug/tracing/tracing_on
+
+
+
+
+
+./fork
+
+
+
+
+echo "Start recordng ftrace data"
+pause "Press any key to stop..."
+echo "Recording stopped..."
+echo 0 > /sys/kernel/debug/tracing/tracing_on
+
+echo "copying [+]"
+time cp /sys/kernel/debug/tracing/trace ~/
+echo "copying [-]"
+
+FileDate=`date +%Y%m%d_%H%M%S`
+
+tar zcvf  "$FileDate".tar.gz trace
+tftp_push "$FileDate".tar.gz
+

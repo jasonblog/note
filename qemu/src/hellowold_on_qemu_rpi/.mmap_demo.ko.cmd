232b54e3ffc072b9d26e8d432179d4863f553e53
gigi <gigi>
2016-03-30 00:01:32 +0800

    Update documentation

---
+++ qemu/src/hellowold_on_qemu_rpi/.mmap_demo.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/shihyu/testtest/mmap_demo.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/shihyu/testtest/mmap_demo.ko /home/shihyu/testtest/mmap_demo.o /home/shihyu/testtest/mmap_demo.mod.o
---
+++ qemu/src/hellowold_on_qemu_rpi/.tmp_versions/mmap_demo.mod
@@ -0,0 +1,2 @@
+/home/shihyu/testtest/mmap_demo.ko
+/home/shihyu/testtest/mmap_demo.o

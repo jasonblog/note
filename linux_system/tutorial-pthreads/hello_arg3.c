21a95ce031a2742ef075b527ea082ddd05c61a3c
gigi <gigi>
2016-05-15 23:41:53 +0800

    Update documentation

---
+++ linux_system/tutorial-pthreads/hello_arg3.c
@@ -0,0 +1,32 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define NUM_THREADS     8
+
+void *PrintHello(void *threadid)
+{
+    long taskid;
+    sleep(1);
+    taskid = *(long *)threadid;
+    printf("Hello from thread %ld\n", taskid);
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t threads[NUM_THREADS];
+    int rc;
+    long t;
+
+    for(t=0;t<NUM_THREADS;t++) {
+        printf("Creating thread %ld\n", t);
+        rc = pthread_create(&threads[t], NULL, PrintHello, (void *) &t);
+        if (rc) {
+            printf("ERROR; return code from pthread_create() is %d\n", 
+rc);
+            exit(-1);
+        }
+    }
+
+    pthread_exit(NULL);
+}

213ef0888b4e30794cd93d244092860e2502340d
gigi <gigi>
2016-06-15 23:14:33 +0800

    Update documentation

---
+++ linux_driver/src/mmap_driver/mmap_kernel.mod.c
@@ -0,0 +1,42 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x96cec1da, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x977a520, __VMLINUX_SYMBOL_STR(debugfs_remove) },
+	{ 0x4218e085, __VMLINUX_SYMBOL_STR(debugfs_create_file) },
+	{ 0x35ffcbfb, __VMLINUX_SYMBOL_STR(__get_page_tail) },
+	{ 0x4c9d28b0, __VMLINUX_SYMBOL_STR(phys_base) },
+	{ 0x69acdf38, __VMLINUX_SYMBOL_STR(memcpy) },
+	{ 0x754d539c, __VMLINUX_SYMBOL_STR(strlen) },
+	{ 0x9b388444, __VMLINUX_SYMBOL_STR(get_zeroed_page) },
+	{ 0xa4d5abf7, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
+	{ 0x643c4c76, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
+	{ 0x37a0cba, __VMLINUX_SYMBOL_STR(kfree) },
+	{ 0x4302d0eb, __VMLINUX_SYMBOL_STR(free_pages) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "B69739EF5B6409543CD6B84");

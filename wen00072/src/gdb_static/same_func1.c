275f83f2dd0267e7281aae2ecf672573071d4055
gigi <gigi>
2016-10-10 15:39:34 +0800

    update

---
+++ wen00072/src/gdb_static/same_func1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#if OP1
+void func1(void)
+{
+    printf("%s: %s_OP1\n", __FILE__, __PRETTY_FUNCTION__);
+}
+#else
+void func1(void)
+{
+    printf("%s: %s_NOT_OP1\n", __FILE__, __PRETTY_FUNCTION__);
+}
+#endif

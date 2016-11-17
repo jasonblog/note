c13044188eb880243f644cfff65ffc4b6983f973
gigi <gigi>
2016-11-17 22:36:46 +0800

    Update documentation

--- gitbook/plugins/gitbook-plugin-maxiang/maxiang.js
+++ gitbook/plugins/gitbook-plugin-maxiang/maxiang.js
@@ -20,16 +20,17 @@ require(["gitbook"], function(gitbook) {
 
         html += '</div>';
 
-        var nav = document.createElement("div");
-        nav.className = "right-nav";
-        nav.id = "js_right_nav";
-        nav.innerHTML = html;
+        //var nav = document.createElement("div");
+        //nav.className = "right-nav";
+        // nav.id = "js_right_nav";
+        //nav.innerHTML = html;
 
-        document.body.querySelector(".page-wrapper").appendChild(nav);
+        //document.body.querySelector(".page-wrapper").appendChild(nav);
 
 
-        var navLinks = document.body.querySelectorAll("#js_right_nav a");
+        // var navLinks = document.body.querySelectorAll("#js_right_nav a");
 
+        /*
         //绑定事件
         nav.onclick = function(event){
 
@@ -44,6 +45,7 @@ require(["gitbook"], function(gitbook) {
         		event.target.className = "select";
         	}
         }
+        */
 
         document.querySelector("#js_right_nav .title .arrow").onclick = function(){
 

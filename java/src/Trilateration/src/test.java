68096732a257c759e35148e0f22dd873d79edc2c
gigi <gigi>
2017-09-08 23:44:49 +0800

    Update documentation

--- java/src/Trilateration/src/test.java
+++ java/src/Trilateration/src/test.java
@@ -3,11 +3,10 @@ public class test
     public static void main(String[] args)
     {
         try {
-
-            double[][] positions = new double[][] {{1.0, 1.0, 1.0}, {3.0, 1.0, 1.0}, {2.0, 2.0, 1.0}};
-            double[] distances = new double[] {1.0, 1.0, 1.0};
-            double[] expectedPosition = new double[] {2.0, 1.0, 1.0};
-            double acceptedDelta = 0.0001;
+            double[][] positions = new double[][] {{1.0, 1.0}, {3.0, 1.0}};
+            double[] distances = new double[] {1.0, 1.0};
+            double[] expectedPosition = new double[] {2.0, 1.0};
+            double acceptedDelta = 0.5;
             new TrilaterationTest(positions, distances, expectedPosition, acceptedDelta);
         } catch (Exception ex) {
             System.out.println("something wrong");

e1b8909763b9ec37ed2783e378a1b69bc62b9c1f
jason_yao <jason_yao>
2018-06-16 01:08:21 +0800

    update

--- linux_system/src/羅習五/ch12/isItSupportC11thread.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(int argc, char** argv) {
-    #ifdef __STDC_NO_THREADS__ 
-    printf("NO theading support\n");
-    #else
-    printf("have threading support\n");
-    #endif
-}

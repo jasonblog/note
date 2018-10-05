4a9cc2c444017e14e0387af5d801547b146d9a32
jason_yao <jason_yao>
2018-10-06 01:37:39 +0800

    update

---
+++ opencv/src/opencv_native_androidstudio_arm64-v8a/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,35 @@
+#include <jni.h>
+#include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <android/log.h>
+
+#ifndef LOG_TAG
+#define LOG_TAG "native-lib"
+#endif
+
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL, LOG_TAG, __VA_ARGS__)
+
+
+using namespace std;
+using namespace cv;
+
+extern "C"
+{
+void JNICALL Java_ch_hepia_iti_opencvnativeandroidstudio_MainActivity_salt(JNIEnv *env, jobject instance,
+                                                                           jlong matAddrGray,
+                                                                           jint nbrElem) {
+    LOGI("YAO salt[+]");
+    Mat &mGr = *(Mat *) matAddrGray;
+    for (int k = 0; k < nbrElem; k++) {
+        int i = rand() % mGr.cols;
+        int j = rand() % mGr.rows;
+        mGr.at<uchar>(j, i) = 255;
+    }
+}
+}

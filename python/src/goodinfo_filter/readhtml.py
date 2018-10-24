31a769f071f54694b98eb985261e1c1adba76da8
jason_yao <jason_yao>
2018-10-25 00:31:14 +0800

    update

--- python/src/goodinfo_filter/readhtml.py
+++ python/src/goodinfo_filter/readhtml.py
@@ -17,8 +17,13 @@ soup = BeautifulSoup(text.replace('&nbsp;', '').replace('　',''), 'lxml')
 df = pd.read_html(str(soup))
 
 for index, row in df[0].iterrows():
+    k = float(re.findall(r"\d+\.?\d*", row['K值(月)'])[0])
+    d = float(re.findall(r"\d+\.?\d*", row['D值(月)'])[0])
+    print(k)
+    print(d)
     #print(row['D值(月)'])
-    print(row['K值(月)'])
+    #print(row['K值(月)'])
+    #print(type(row['K值(月)']))
     '''
     print(type(item))
     for k, d in item['K值(月)'], item['D值(月)']:
